4fc0ea0d16a45480b11575407126fd8c9862a5e3
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-08-03 23:22:12 +0200

    add dashboard, notifications and projectlist components

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const ProjestList = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Dawid K.</p>
+                    <p className="grey-text">5th August</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Dawid K.</p>
+                    <p className="grey-text">5th August</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Dawid K.</p>
+                    <p className="grey-text">5th August</p>
+                </div>
+            </div>
+
+        </div>
+
+
+    )
+}
+
+export default ProjestList
