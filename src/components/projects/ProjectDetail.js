3b67b41a5584e283b293f47d5c954fd5c51b5ec2
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-08-04 22:11:27 +0200

    add new components

---
+++ src/components/projects/ProjectDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectDetail = () => {
+    return (
+        <div className="container section project-details">
+
+        </div>
+    )
+}
+
+export default ProjectDetail
