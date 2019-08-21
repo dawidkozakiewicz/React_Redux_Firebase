2381dcd9b15cd38c78120453703edfade8343905
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-08-21 23:23:58 +0200

    add dispatch

---
+++ src/store/actions/projectActions.js
@@ -0,0 +1,16 @@
+// export const createProject = (project) => {
+//     return {
+//         type: 'ADD_PROJECT',
+//         project: project
+//     }
+// }
+
+
+export const createProject = (project) => {
+    return (dispatch, getState) => {
+
+        // make async call to database
+
+        dispatch({ type: 'CREATE_PROJECT', project: project })
+    }
+};
