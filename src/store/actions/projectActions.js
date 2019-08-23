1ff1e117814aab98cf3794dedca877a69d40870b
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-08-23 20:49:33 +0200

    send documents to firebase

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -8,10 +8,19 @@
 
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
-
-
         // make async call to database
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstname: 'Dawid',
+            authorLastName: 'Kozakiewicz',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project })
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+        })
 
-        dispatch({ type: 'CREATE_PROJECT', project: project })
     }
 };
