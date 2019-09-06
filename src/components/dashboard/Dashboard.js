b8eaee69000f76ddeb39b34f68fa08ce237ae8a3
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-09-06 22:55:06 +0200

    add render notifications from firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,12 +6,11 @@ import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
 
+
+
 class Dashboard extends Component {
   render() {
-
-    // console.log(this.props);
-    const { projects, auth } = this.props;
-
+    const { projects, auth, notifications } = this.props;
     if (!auth.uid) return <Redirect to='/signin' />
 
     return (
@@ -21,7 +20,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
@@ -30,16 +29,18 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state)
+  // console.log(state);
   return {
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
   }
 }
 
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'projects' }
+    { collection: 'projects' },
+    { collection: 'notifications', limit: 3 }
   ])
 )(Dashboard)
